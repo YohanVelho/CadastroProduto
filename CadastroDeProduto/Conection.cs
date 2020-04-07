ef5ec80bee20b497b6eec89364f10387e2c3a861
Yohan Velho <yohanvelho@gmail.com>
2020-04-07 01:52:46 -0300

    projeto iniciado

---
+++ CadastroDeProduto/Conection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace CadastroDeProduto
+{
+    class Conection
+    {
+        public string connstring = "Server=127.0.0.1;Port=5432;Database=DbCadastro;User Id=postgres;Password=181271;";
+
+    }
+}
